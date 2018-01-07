16be219184b38bfbee194f52fcce8b6193aac276
Falko Schumann <falko.schumann@muspellheim.de>
2018-01-07 13:22:38 +0100

    API added to alarm clock

--- src/main/java/de/muspellheim/alarmclock/portal/AlarmClockController.java
+++ src/main/java/de/muspellheim/alarmclock/portal/AlarmClockController.java
@@ -5,11 +5,20 @@
 
 package de.muspellheim.alarmclock.portal;
 
+import de.muspellheim.util.*;
+import javafx.application.*;
 import javafx.fxml.*;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 
+import java.time.*;
+import java.time.format.*;
+
 public class AlarmClockController {
+
+    public final Event<LocalTime> onStart = new Event<>();
+
+    public final Action onStop = new Action();
 
     @FXML
     private Pane view;
@@ -31,14 +40,41 @@ public class AlarmClockController {
     }
 
     @FXML
-    private void toggleOnOff() {
-        if (onOff.isSelected()) {
-            onOff.setStyle("-fx-base: '#ff4141'");
-            remainingTime.setVisible(true);
-        } else {
-            onOff.setStyle(null);
-            remainingTime.setVisible(false);
+    private void validateWakeupTime() {
+        try {
+            LocalTime.parse(wakeupTime.getText());
+            onOff.setDisable(false);
+        } catch (DateTimeParseException e) {
+            onOff.setDisable(true);
         }
+    }
+
+    @FXML
+    private void toggleOnOff() {
+        if (onOff.isSelected())
+            start();
+        else
+            stop();
+    }
+
+    public void updateCurrentTime(LocalDateTime time) {
+        Platform.runLater(() -> currentTime.setText(time.format(DateTimeFormatter.ofLocalizedTime(FormatStyle.MEDIUM))));
+    }
+
+    public void remainingTime(LocalTime time) {
+        Platform.runLater(() -> remainingTime.setText(time.format(DateTimeFormatter.ofLocalizedTime(FormatStyle.MEDIUM))));
+    }
+
+    public void start() {
+        onOff.setStyle("-fx-base: '#ff4141'");
+        remainingTime.setVisible(true);
+        onStart.send(LocalTime.parse(wakeupTime.getText()));
+    }
+
+    public void stop() {
+        onOff.setStyle(null);
+        remainingTime.setVisible(false);
+        onStop.trigger();
     }
 
 }
