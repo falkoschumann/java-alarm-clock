3163bf3f02a81734d4ce30a6ec4071866c115d13
Falko Schumann <falko.schumann@muspellheim.de>
2018-12-01 16:21:21 +0100

    Clock implemented

---
+++ src/main/java/de/muspellheim/alarmclock/portal/Clock.java
@@ -0,0 +1,37 @@
+/*
+ * Alarm Clock
+ * Copyright (c) 2018 Falko Schumann
+ */
+
+package de.muspellheim.alarmclock.portal;
+
+import de.muspellheim.util.*;
+
+import java.time.*;
+import java.util.*;
+import java.util.concurrent.*;
+
+public class Clock {
+
+    private final Event<LocalDateTime> onCurrentTime = new Event<>();
+
+    public Clock() {
+        var timer = new Timer("Clock", true);
+        var task = new ClockTask();
+        timer.scheduleAtFixedRate(task, 0, TimeUnit.SECONDS.toMillis(1));
+    }
+
+    public Event<LocalDateTime> onCurrentTime() {
+        return onCurrentTime;
+    }
+
+    private class ClockTask extends TimerTask {
+
+        @Override
+        public void run() {
+            onCurrentTime.send(LocalDateTime.now());
+        }
+
+    }
+
+}
