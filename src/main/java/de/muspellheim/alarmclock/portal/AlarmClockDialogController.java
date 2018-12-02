67157856cb2e9bb3bef476daff70ee98c6d4b1b5
Falko Schumann <falko.schumann@muspellheim.de>
2018-12-02 13:10:41 +0100

    Alarm clock dialog implemented

--- src/main/java/de/muspellheim/alarmclock/portal/AlarmClockDialogController.java
+++ src/main/java/de/muspellheim/alarmclock/portal/AlarmClockDialogController.java
@@ -5,12 +5,19 @@
 
 package de.muspellheim.alarmclock.portal;
 
+import de.muspellheim.util.*;
 import javafx.fxml.*;
 import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 
+import java.time.*;
+import java.time.format.*;
+
 public class AlarmClockDialogController {
+
+    private final Event<LocalTime> onStartRequested = new Event<>();
+    private final Action onStopRequested = new Action();
 
     @FXML
     private Pane view;
@@ -25,15 +32,50 @@ public class AlarmClockDialogController {
     private TextField wakeUpTimeTextField;
 
     @FXML
-    private ToggleButton onOffButton;
+    private ToggleButton startStopButton;
 
     public Parent getView() {
         return view;
     }
 
-    @FXML
-    private void toggleOnOff() {
+    public void updateCurrentTime(LocalDateTime time) {
+        String s = time.format(DateTimeFormatter.ofLocalizedTime(FormatStyle.MEDIUM));
+        currentTimeLabel.setText(s);
+    }
 
+    public void updateRemainingTime(Duration time) {
+        LocalTime t = LocalTime.of(time.toHoursPart(), time.toMinutesPart(), time.toSecondsPart());
+        DateTimeFormatter formatter = DateTimeFormatter.ofLocalizedTime(FormatStyle.MEDIUM);
+        String s = t.format(formatter);
+        currentTimeLabel.setText(s);
+    }
+
+    public void wakeUpTimeReached() {
+        remainingTimeLabel.setVisible(false);
+        startStopButton.setSelected(false);
+    }
+
+    @FXML
+    private void startStopButtonClicked() {
+        if (startStopButton.isSelected()) {
+            remainingTimeLabel.setVisible(true);
+            remainingTimeLabel.setText("");
+
+            DateTimeFormatter formatter = DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT);
+            LocalTime time = LocalTime.parse(wakeUpTimeTextField.getText(), formatter);
+            onStartRequested.send(time);
+        } else {
+            remainingTimeLabel.setVisible(false);
+            onStopRequested.trigger();
+        }
+    }
+
+    public Event<LocalTime> onStartRequested() {
+        return onStartRequested;
+    }
+
+    public Action onStopRequested() {
+        return onStopRequested;
     }
 
 }
